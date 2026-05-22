b0a16de0f2ce3eb748131b12071999f1c5f52bae
sahankumar6699-dev <sahankumar6699@gmail.com>
2026-05-22 12:31:35 +0530

    fix: pass correct ToolCategory object instead of string literal to ToolCategoryPage

--- app/converter/page.tsx
+++ app/converter/page.tsx
@@ -1,5 +1,6 @@
 import { ToolCategoryPage } from "@/components/tool-category-page";
+import { toolCategories } from "@/lib/tools";
 
 export default function ConverterPage() {
-  return <ToolCategoryPage category="converter" />;
+  return <ToolCategoryPage category={toolCategories.converter} />;
 }
